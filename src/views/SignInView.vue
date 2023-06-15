7835b93df17bbaf5c1d2a550fd7581f5546e95fe
KikosS41 <killian.legendre41@gmail.com>
2023-06-15 09:29:49 +0200

    generate project

---
+++ src/views/SignInView.vue
@@ -0,0 +1,16 @@
+<template>
+    <SignIn/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import SignIn from '@/components/SignIn.vue'; // @ is an alias to /src
+
+@Options({
+  components: {
+    SignIn,
+  },
+
+})
+export default class LoginView extends Vue {}
+</script>
